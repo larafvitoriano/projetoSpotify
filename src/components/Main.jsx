8522e782fdc896c74c50492b3cbb9da36154fd95
Lara Fernanda <larafvitoriano@gmail.com>
2025-02-15 23:24:29 -0300

    feat: configurando cabeçalho e conteúdo principal

---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="main-text">
+        <h2>Artistas populares</h2>
+        <a className="main-link" href="/">
+          Mostrar tudo
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
